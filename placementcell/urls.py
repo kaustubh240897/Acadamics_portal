2d14b3ea5a6651ffe3786cc5232b8c4d4da8bb12
unknown <kaustubh.dwi@gmail.com>
2018-10-06 01:44:33 +0530

    updating for register and signup

--- placementcell/urls.py
+++ placementcell/urls.py
@@ -11,13 +11,14 @@
 #     url(r'^login/$', login_page, name='login'),
 # ]
 from django.conf.urls import url, include
+from django.contrib import admin
 from django.contrib.auth import views as auth_views
-
 from .core import views as core_views
 
 
 urlpatterns = [
     url(r'^$', core_views.home, name='home'),
+    url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.LoginView.as_view(template_name="login.html"), {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name="logout.html"), {'next_page': 'login'}, name='logout'),
     url(r'^signup/$', core_views.signup, name='signup'),

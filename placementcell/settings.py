657a56ec550cc1774752d80593f473f860579603
unknown <kaustubh.dwi@gmail.com>
2018-10-06 01:18:19 +0530

    updating settings.py

--- placementcell/settings.py
+++ placementcell/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = '201652008@iiitvadodara.ac.in'
-EMAIL_HOST_PASSWORD = 'math12345'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True

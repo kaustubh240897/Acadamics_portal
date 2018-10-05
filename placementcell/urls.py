f8f19df5ef515e59838c74a750fb2692bc3f4218
unknown <kaustubh.dwi@gmail.com>
2018-10-06 00:29:11 +0530

    update the url.py

--- placementcell/urls.py
+++ placementcell/urls.py
@@ -1,12 +1,27 @@
-from django.conf.urls import url
-from django.contrib import admin
-from .views import home_page
-from . import views
-from .views import login_page
+# from django.conf.urls import url
+# from django.contrib import admin
+# from .views import home_page
+# from . import views
+# from .views import login_page
+#
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+#     url(r'^$', home_page, name='home_page'),
+#     url(r'^Recruiter_register/$', views.Recruiter_registerView.as_view(), name='register'),
+#     url(r'^login/$', login_page, name='login'),
+# ]
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
+from .core import views as core_views
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', home_page, name='home_page'),
-    url(r'^Recruiter_register/$', views.Recruiter_registerView.as_view(), name='register'),
-    url(r'^login/$', login_page, name='login'),
+    url(r'^$', core_views.home, name='home'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name="login.html"), {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name="logout.html"), {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
+    url(r'^account_activation_sent/$', core_views.account_activation_sent, name='account_activation_sent'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        core_views.activate, name='activate'),
 ]
--- placementcell/views.py
+++ placementcell/views.py
@@ -1,81 +1,83 @@
-from django.http import HttpResponse, request
-from django.shortcuts import render, redirect
-from django.views import generic
-from django.contrib.auth import authenticate,login
-from django.views.generic import View
-from .forms import Recruiter_register_form,LoginForm
+# from django.http import HttpResponse, request
+# from django.shortcuts import render, redirect
+# from django.views import generic
+# from django.contrib.auth import authenticate,login
+# from django.views.generic import View
+# from .forms import Recruiter_register_form,LoginForm
+#
+#
+# def home_page(request):
+#     context = {"content": "Welcome to home page"}
+#
+#     return render(request, 'home_page.html', context)
+#
+#
+# class Recruiter_registerView(View):
+#     form_class = Recruiter_register_form
+#     template_name = 'Recruiter_registration.html'
+#
+#     # display blank form
+#     def get(self,request):
+#         form = self.form_class(None)
+#         return render(request, self.template_name, {'form': form})
+#     #process from data
+#     def post(self,request):
+#         form=self.form_class(request.POST)
+#
+#         if form.is_valid():
+#
+#             user=form.save(commit=False)
+#
+#             #clean and normalize data
+#             username = form.cleaned_data['username']
+#             organisation_name=form.cleaned_data['organisation_name']
+#             email=form.cleaned_data['email']
+#             password=form.cleaned_data['password']
+#             first_name=form.cleaned_data['first_name']
+#             last_name=form.cleaned_data['last_name']
+#             phone_number=form.cleaned_data['phone_number']
+#
+#             user.set_password(password)
+#
+#             user.save()
+#
+#             # returns user objects if credentials are correct
+#             user = authenticate(request, username=username, password=password)
+#
+#             if user is not None and user.is_active:
+#                 login(request, user)
+#                 return redirect("/")
+#
+#         return render(request, self.template_name, {'form': form})
+#
+#
+# def login_page(request, username=None):
+#     form= LoginForm(request.POST or None)
+#
+#     context = {
+#         "form": form
+#     }
+#     print("user logged in")
+#     #print(request.user.is_authenticated())
+#     if form.is_valid():
+#         print(form.cleaned_data)
+#         username=form.cleaned_data.get("username")
+#         password=form.cleaned_data.get("password")
+#         user=authenticate(request, username=username, password=password)
+#         print(user)
+#         if user is not None:
+#             #print(request.user.is_authenticated())
+#             login(request, user)
+#             # Redirect to a success page.
+#             # context['form'] = LoginForm()
+#             return redirect("/")
+#         else:
+#             print("Error")
+#             # Return an 'invalid login' error message.
+#
+#
+#     return render(request, "login.html", context)
 
 
-def home_page(request):
-    context = {"content": "Welcome to home page"}
-
-    return render(request, 'home_page.html', context)
-
-
-class Recruiter_registerView(View):
-    form_class = Recruiter_register_form
-    template_name = 'Recruiter_registration.html'
-
-    # display blank form
-    def get(self,request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form': form})
-    #process from data
-    def post(self,request):
-        form=self.form_class(request.POST)
-
-        if form.is_valid():
-
-            user=form.save(commit=False)
-
-            #clean and normalize data
-            username = form.cleaned_data['username']
-            organisation_name=form.cleaned_data['organisation_name']
-            email=form.cleaned_data['email']
-            password=form.cleaned_data['password']
-            first_name=form.cleaned_data['first_name']
-            last_name=form.cleaned_data['last_name']
-            phone_number=form.cleaned_data['phone_number']
-
-            user.set_password(password)
-
-            user.save()
-
-            # returns user objects if credentials are correct
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None and user.is_active:
-                login(request, user)
-                return redirect("/")
-
-        return render(request, self.template_name, {'form': form})
-
-
-def login_page(request, username=None):
-    form= LoginForm(request.POST or None)
-
-    context = {
-        "form": form
-    }
-    print("user logged in")
-    #print(request.user.is_authenticated())
-    if form.is_valid():
-        print(form.cleaned_data)
-        username=form.cleaned_data.get("username")
-        password=form.cleaned_data.get("password")
-        user=authenticate(request, username=username, password=password)
-        print(user)
-        if user is not None:
-            #print(request.user.is_authenticated())
-            login(request, user)
-            # Redirect to a success page.
-            # context['form'] = LoginForm()
-            return redirect("/")
-        else:
-            print("Error")
-            # Return an 'invalid login' error message.
-
-
-    return render(request, "login.html", context)
 
 
